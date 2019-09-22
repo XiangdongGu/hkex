560130a5d0a64ea7bc93d4c49076d3401519ba05
Xiangdong Gu <ustcgxd@gmail.com>
2019-09-22 20:57:03 +0800

    add message

--- hkex_data.R
+++ hkex_data.R
@@ -9,4 +9,7 @@ equities <- get_equities()
 con <- psql_con()
 
 # Loadd all codes in equities
-for (code in equities$code) load_stock(con, code)
+for (code in equities$code) {
+  msg <- load_stock(con, code)
+  cat(sprintf("%s for %s", msg, code))
+}
