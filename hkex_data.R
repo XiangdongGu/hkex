dbcb9e30592a16d82bcb01bed748bf350b20cadd
Xiangdong Gu <ustcgxd@gmail.com>
2019-09-22 21:07:23 +0800

    re-try if error

--- hkex_data.R
+++ hkex_data.R
@@ -13,6 +13,10 @@ codes <- codes[order(runif(length(codes)))]
 
 # Loadd all codes in equities
 for (code in codes) {
-  msg <- load_stock(con, code)
+  msg <- try(load_stock(con, code))
+  if (inherits(msg, "try-error")) {
+    Sys.sleep(3)
+    msg <- try(load_stock(con, code))
+  }
   cat(sprintf("%s for %s\n", msg, code))
 }
